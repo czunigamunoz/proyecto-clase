f7048aa49a8d729329bb18ff0fcc104ba16fa0e6
czm <cezunigamunoz@gmail.com>
2022-05-09 15:47:31 -0500

    added splah

--- app/src/main/java/com/colombina/MainActivity.java
+++ app/src/main/java/com/colombina/MainActivity.java
@@ -1,14 +1,31 @@
 package com.colombina;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, 5000);
     }
 }
